f11d0bf3397900702c9ad0659536984194ddb1e7
MartinMNapan <MY_NAME@example.com>
2022-08-16 00:48:05 -0500

    Part3

---
+++ app/src/main/java/pe/edu/idat/apppatitasidatccm/view/fragments/ListaMascotaFragment.kt
@@ -0,0 +1,19 @@
+package pe.edu.idat.apppatitasidatccm.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pe.edu.idat.apppatitasidatccm.R
+
+class ListaMascotaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_mascota, container, false)
+    }
+}
